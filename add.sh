83b3ad42e825d1e8a3da5cdf6e0354c1d71e826a
nasuda <nasuda908@gmail.com>
2023-08-12 08:36:15 +0900

    add sh

--- add.sh
+++ add.sh
@@ -4,4 +4,30 @@ echo $1 >> $2
 
 cat -n $2 | sort -rn -k 2 | head -n 5
 
-echo "now: $1yen"
+point=$1
+border=3000
+
+filename=$2
+
+case "$filename " in
+  3*)
+    border=3000
+    ;;
+  5*)
+    border=5000
+    ;;
+  10:) 
+    border=10000
+    ;;
+esac
+
+echo "You played $border yen course."
+echo "now: $point yen"
+
+if [ "$point" -gt "$border" ]; then
+  echo "OK!"
+else
+  echo "NO!"
+fi
+
+echo "Today is $(date '+%Y-%m-%d')"
